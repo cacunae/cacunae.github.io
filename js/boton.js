491e7c6b47411083a9731e73534944788bce6dfc
Cristobal <nakar.zeta@gmail.com>
2019-10-16 15:42:51 -0300

    primer comit

---
+++ js/boton.js
@@ -0,0 +1,8 @@
+let boton = document.getElementById('midBanner');
+let nav = document.getElementById('blog');
+
+boton.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+})
+
+
